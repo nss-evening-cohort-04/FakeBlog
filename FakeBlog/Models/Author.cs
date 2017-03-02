2160f3c783a59fc1c96f395d6eb4bbe8bbfe004c
jessica kilby <jessicakilby@gmail.com>
2017-03-02 15:46:05 -0600

    fakeBlog erd and classes

---
+++ FakeBlog/Models/Author.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.Models
+{
+    public class Author
+    {
+        [Key] 
+        public int AuthorId { get; set; } 
+
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        [MaxLength(30)]
+        public string Email { get; set; }
+
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        public ApplicationUser BaseUser { get; set; } 
+
+        public List<Portfolio> Portfolios { get; set; } 
+    }
+}
---
+++ FakeBlog/Models/JoinAuthorsToPosts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.Models
+{
+    public class JoinAuthorsToPosts
+    {
+        
+    }
+}
