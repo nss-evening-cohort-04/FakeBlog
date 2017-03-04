61efc93b27f1638c2fc3e7877b197f3ffdf18445
Alex Saber <alexsaber89@gmail.com>
2017-03-04 13:40:58 -0600

    add Author entity

---
+++ FakeBlog/Models/Author.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.Models
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorID { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string AuthorPasswordHash { get; set; }
+
+        public DateTime AuthorJoinDate { get; set; }
+
+        public List<DraftPost> DraftPosts { get; set; } // 1 to many relationship
+
+        public List<PublishedPost> PublishedPosts { get; set; } // 1 to many relationship
+    }
+}
