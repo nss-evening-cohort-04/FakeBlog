45201123cf9a4dc1bb422b68f9c43154d3ecbc5d
Dustin Reed <dmreedy1@gmail.com>
2017-03-04 11:59:25 -0600

    added post and author models

---
+++ FakeBlog/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FakeBlog.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+        public bool IsDraft { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string URL { get; set; }
+
+    }
+}
