d32819ec73abbd7ec15e0c4dc13c723465db0620
Justin Leggett <justin@codingrookie.com>
2017-03-04 11:56:10 -0600

    created layout for database and implemented

---
+++ FakeBlog/Models/Author.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.Models
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorId { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string Username { get; set; }
+
+        public string  Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public ICollection<Post> Posts { get; set; } // 1 to many 
+    }
+}
