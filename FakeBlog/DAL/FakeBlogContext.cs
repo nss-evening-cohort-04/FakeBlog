f59ff73b79379d2213a4b857ac615adc74ac5a0b
Alex Saber <alexsaber89@gmail.com>
2017-03-14 19:40:43 -0500

    initialize FakeBlogContext and IFakeBlogRepository, install Moq, Entity, and Entity Identity

---
+++ FakeBlog/DAL/FakeBlogContext.cs
@@ -0,0 +1,13 @@
+﻿using FakeBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.DAL
+{
+    public class FakeBlogContext : ApplicationDbContext
+    {
+
+    }
+}
