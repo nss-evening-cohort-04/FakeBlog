c286db8780a5f1460bb946cfd55ea7ad5262c364
Alex Saber <alexsaber89@gmail.com>
2017-03-14 20:57:18 -0500

    create methods for IFakeBlogRepository

--- FakeBlog/DAL/IFakeBlogRepository.cs
+++ FakeBlog/DAL/IFakeBlogRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FakeBlog.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,21 @@ namespace FakeBlog.DAL
     public interface IFakeBlogRepository
     {
         // Users who signup are considered "Authors"
+        void AddAuthor(string authorName, ApplicationUser user);
+
         // Published posts will be viewable by everyone
+        List<Post> GetPublishedPosts(Author authorId);
+
         // Authors will be able to make drafts for blog posts
+        void CreateDraftPost(Author authorId, string postTitle, string postContent);
+
         // Authors will be able to manually publish a draft.
+        void PublishDraftPost(int postId);
+
         // Authors will be able to delete published posts and drafts
+        void DeletePost(int postId);
+
         // Authors will be able to edit a drafts
+        void EditDraftPost(int postId, string editedTitle, string editedContent);
     }
 }
